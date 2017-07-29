7f6731877b2b7fc86e6eb3a7973071816ae6a971
DavidBetteridge <davidabetteridge@gmail.com>
2017-07-29 22:52:42 +0100

    WIP

--- Script.fsx
+++ Script.fsx
@@ -12,17 +12,20 @@ type colour =
 
 type program  = colour[,]
 
-type dp =
+type direction =
     | Right
     | Down
     | Left
     | Up
+
+type dp = direction
 
 type cc =
     | Left
     | Right
 
 type command =
+    | NOP
     | Push
     | Pop
     | Add
@@ -41,6 +44,7 @@ type command =
     | OutNumber
     | OutChar
 
+let modulo n m = ((n % m) + m) % m
 
 let colourToCell (color:Color) : colour = 
     match int(color.R), int(color.G), int(color.B) with
@@ -96,36 +100,155 @@ let getMatchingNeighbours x y width height (program:program) =
         }
 
 
+let rec findAdjBlocks program width height toExamine (examined:List<int*int>) =
+    match toExamine with
+    | [] -> examined
+    | (x,y)::xs -> let possibleOptions = getMatchingNeighbours x y width height program
+                       
+                   // Make sure we haven't already examined them
+                   let possibleOptions' = possibleOptions |> Seq.where(fun (x, y) -> not(examined |> List.exists(fun (x', y') -> x' = x && y' = y)))
+                       
+                   // Make sure we don't already have them in our list to examine
+                   let possibleOptions'' = possibleOptions' 
+                                                    |> Seq.where(fun (x, y) -> not(toExamine |> List.exists(fun (x', y') -> x' = x && y' = y)))
+                                                    |> Seq.toList
+
+                   // Add to the list
+                   let toExamine' = xs |> List.append possibleOptions''
+
+                   findAdjBlocks program width height toExamine' ((x,y) :: examined)
+
+
 let countPixelsInBlock program x y width height : int =
-    
-    let rec findAdjBlocks toExamine (examined:List<int*int>) =
-        match toExamine with
-        | [] -> examined
-        | (x,y)::xs -> let possibleOptions = getMatchingNeighbours x y width height program
-                       
-                       // Make sure we haven't already examined them
-                       let possibleOptions' = possibleOptions |> Seq.where(fun (x, y) -> not(examined |> List.exists(fun (x', y') -> x' = x && y' = y)))
-                       
-                       // Make sure we don't already have them in our list to examine
-                       let possibleOptions'' = possibleOptions' 
-                                                        |> Seq.where(fun (x, y) -> not(toExamine |> List.exists(fun (x', y') -> x' = x && y' = y)))
-                                                        |> Seq.toList
-
-                       // Add to the list
-                       let toExamine' = xs |> List.append possibleOptions''
-
-                       findAdjBlocks toExamine' ((x,y) :: examined)
-        
-    let toExamine = [x,y]
-    let examined = findAdjBlocks toExamine []
+    let examined = findAdjBlocks program width height [x,y] []
     examined |> Seq.length
 
-let getNextCommand x y (dp:dp) (cc:cc) program : (command * dp * cc * int * int) = 
-    []
+let furthestInDirection program x y width height (dp:dp) : (int*int) =
+    let examined = findAdjBlocks program width height [x,y] []
+    
+    match dp with
+    | dp.Right -> examined |> List.maxBy(fun (x,y) -> x)  
+    | dp.Left -> examined |> List.minBy(fun (x,y) -> x)  
+    | dp.Up -> examined |> List.minBy(fun (x,y) -> y)  
+    | dp.Down -> examined |> List.maxBy(fun (x,y) -> y)  
+    
+let rec findEdgeOfBlock width height (program:program) (direction:direction) x y   = 
+    //Keep walking until we hit an edge, or a block of a different colour
+    let colourToMatch = program.[x,y]
+    let next = findEdgeOfBlock width height program direction
+
+    match direction with
+    | direction.Left ->  if (x = 0 || program.[x - 1,y] <> colourToMatch) then x,y else next (x - 1) y  
+    | direction.Right ->  if (x = width - 1 || program.[x + 1,y] <> colourToMatch) then  x,y else next (x + 1) y  
+    | direction.Up ->  if (y = 0 || program.[x,y - 1] <> colourToMatch) then x,y else next x (y - 1)  
+    | direction.Down ->  if (y = height - 1 || program.[x,y + 1] <> colourToMatch) then x,y else next x (y + 1)  
+
+let moveIntoNextBlock width height (program:program) (dp:dp) startX startY   = 
+    let rec next x y = 
+        if program.[startX,startY] <> program.[x,y] then
+            //We have reached a new block
+            Some (x, y)
+        else
+            match dp with
+            | dp.Left ->  if (x = 0) then None else next (x - 1) y  
+            | dp.Right ->  if (x = width - 1) then None else next (x + 1) y  
+            | dp.Up ->  if (y = 0) then None else next x (y - 1)  
+            | dp.Down ->  if (y = height - 1) then None else next x (y + 1)  
+
+    //Keep walking until we hit an edge, or a block of a different colour
+    next startX startY
+    
+
+    
+let directionFromDPandCC (dp:dp) (cc:cc) = 
+    match dp, cc with
+    | dp.Right, cc.Left -> direction.Up
+    | dp.Right, cc.Right -> direction.Down
+    | dp.Down, cc.Left -> direction.Right
+    | dp.Down, cc.Right -> direction.Left
+    | dp.Left, cc.Left -> direction.Down
+    | dp.Left, cc.Right -> direction.Up
+    | dp.Up, cc.Left -> direction.Left
+    | dp.Up, cc.Right -> direction.Right
 
 
+let toggle = function 
+            | cc.Left -> Right
+            | cc.Right -> Left
 
-let image = new Bitmap("C:\Code\piet\Piet.net\Piet_hello_big.png")
+let moveDPClockwise = function
+                    | dp.Left -> dp.Up
+                    | dp.Up -> dp.Right
+                    | dp.Right -> dp.Down
+                    | dp.Down -> dp.Left
+
+let getNextCommand program width height x y (dp:dp) (cc:cc) = 
+    
+    let rec worker (dp:dp) (cc:cc) attempt = 
+
+        let x', y' = furthestInDirection program x y width height dp
+        let dir = directionFromDPandCC dp cc
+        let x'', y'' = findEdgeOfBlock width height program dir x' y'   
+        let next = moveIntoNextBlock width height program dp x'' y''
+
+        let isOk =  match next with
+                    | None -> false //We have hit an edge
+                    | Some(x',y') when program.[x', y'] = colour.Black -> false //We have hit a restriction
+                    | _ -> true //We are ok.
+
+        if (isOk) then
+            Some(next.Value, dp, cc)
+        else
+            match attempt with
+            | 0 | 2 | 4 | 6-> worker dp (toggle cc) (attempt + 1)
+            | 1 | 3 | 5 | 7-> worker (moveDPClockwise dp) (toggle cc) (attempt + 1)
+            | _ -> None
+
+    worker dp cc 0
+
+let decodeCommand (fromColour:colour) (toColour:colour) : command =
+    let (h1,l1) = match fromColour with
+                  | Colour(h,l) -> (h,l)
+                  | _ -> failwith("FromColour Must be a colour")
+
+    let (h2,l2) = match toColour with
+                  | Colour(h,l) -> (h,l)
+                  | _ -> failwith("ToColour Must be a colour")
+
+    let hueChange = modulo (int(h2) - int(h1)) 6
+    let lightnessChange = modulo (int(l2) - int(l1)) 3
+
+    match hueChange, lightnessChange with
+    | 0,0 -> NOP
+    | 0,1 -> Push
+    | 0,2 -> Pop
+    | 1,0 -> Add
+    | 1,1 -> Substract
+    | 1,2 -> Multiple
+    | 2,0 -> Divide
+    | 2,1 -> Mod
+    | 2,2 -> Not
+    | 3,0 -> Greater
+    | 3,1 -> Pointer
+    | 3,2 -> Switch
+    | 4,0 -> Duplicate
+    | 4,1 -> Roll
+    | 4,2 -> InNumber
+    | 5,0 -> InChar
+    | 5,1 -> OutNumber
+    | 5,2 -> OutChar
+    | _,_ -> failwith("Unexpected hue/lightness change")
+
+    
+
+let image = new Bitmap("C:\Code\Piet.net\Piet_hello_big.png")
 let width, height, program = loadImage image 5
 countPixelsInBlock program 0 0 width height 
+
+let dp' = dp.Right
+let cc' = cc.Left
+let next = getNextCommand program width height 0 0 dp' cc'
+let (x,y),dp'',cc'' = next.Value
+let cmd = decodeCommand program.[0,0] program.[x,y] 
+printfn "%A" cmd
 ()
